06ac013d38f71c7d6abb08c40a04585b9cb6d174
Hrutvik Jagtap <hrutvikjagtap51@gmail.com>
2022-08-25 11:08:50 +0530

    Reduce database connection time

--- mainbody/mainbody/settings.py
+++ mainbody/mainbody/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mainbody.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(conn_max_age=60)
 }
 
 
